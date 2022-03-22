de5d55b4b3327a46b3924d387886183215fc9aaf
Avircut <lomoff.slaw@yandex.ru>
2022-03-22 22:11:40 +0700

    version 1.0.0

--- app/js/main.js
+++ app/js/main.js
@@ -17,3 +17,48 @@ $('.burger').on('click', () => {
 $('.burger__menu-close-btn').on('click', () => {
   $('.burger').trigger('click');
 });
+
+
+function animationStart(entries, observer)
+{
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('start');
+    }
+  });
+}
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2
+  };
+
+let observer = new IntersectionObserver(animationStart, options);
+document.querySelectorAll('.slideLeft').forEach( element => {
+  observer.observe(element);
+})
+document.querySelectorAll('.slideBottom').forEach(element => {
+  observer.observe(element);
+})
+document.querySelectorAll('.slideRight').forEach(element => {
+  observer.observe(element);
+})
+
+$('.gallery-mini__slider').slick({
+  dots:false,
+  arrows:true,
+  slidesToShow:3,
+  slidesToScroll:3,
+  responsive:[
+    {
+      breakpoint:660,
+      settings:{
+        slidesToShow:1,
+        slidesToScroll:1,
+        arrows:false,
+        dots:true
+      }
+    }
+  ]
+})
